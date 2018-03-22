98ad316d4403c730513d4fba94a5afcf37f0335b
Benjamin Guest <benguest@gmail.com>
2018-03-22 02:42:40 -0400

    Add Eagle

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@ cd ~/Documents/Arduino
 ln -s $(pwd)/OfficeSign/lib $(pwd)/libraries/OfficeSign
 cd ~/Documents/Arduino/libraries
 git clone git@github.com:adafruit/Adafruit-WS2801-Library.git Adafruit_WS2801
+cd ~/Documents
+ln -s $(pwd)/Arduino/OfficeSign/eagle $(pwd)/eagle/OfficeSign
 echo "Finished"
