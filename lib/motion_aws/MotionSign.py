28d15c516b52020ffc7de7dc72dfb385ef716425
Antall Fernandes <antallfernandes@gmail.com>
2018-07-12 23:48:10 -0400

    Reserved flag set based on google calendar, introduce Google Creds Manager

--- lib/motion_aws/MotionSign.py
+++ lib/motion_aws/MotionSign.py
@@ -25,9 +25,55 @@ eastern = pytz.timezone('US/Eastern')
 os.environ["TZ"]="US/Eastern"
 time.tzset()
 
+class GoogleCredsManager:
+    def __init__(self):
+        self.credentials = None
+
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    def get_credentials(self):
+        if self.credentials is None:
+            print("Fetch creds")
+            credential_path = '/tmp/credentials.json'
+            store = oauth2client.file.Storage(credential_path)
+            self.credentials = store.get()
+        return self.credentials
+
+google_creds_manager = GoogleCredsManager()
 
 def respond(msg=""):
     return msg
+
+
+def __fetch_google_events(room_id, num_events = 2):
+    room_entry = dynamo.get_item(
+            TableName='MotionSign',
+            Key={
+                'room_id': { 'N': room_id }
+            })
+
+    calendar_id = room_entry['Item'].get('calendar_id').get('S')
+
+    credentials = google_creds_manager.get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http, cache_discovery=False)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    eventsResult = service.events().list(
+        calendarId=calendar_id,
+        timeMin=now,
+        maxResults=num_events,
+        singleEvents=True,
+        orderBy='startTime'
+    ).execute()
+    return eventsResult.get('items', [])
+
 
 
 def __get_methods(dynamo, params):
@@ -81,39 +127,43 @@ Method to display reservation details for the room
 """
 def __reserved(dynamo, params):
     room_id = params['room_id']
-    occupied = int(params['occupied'])
+    old_reserved_value = int(params['reserved']) if 'reserved' in params else 0
 
-    dynamo.update_item(
-        TableName='MotionSign',
-        Key={
-            'room_id': {
-                'N': str(room_id)
-            }
-        },
-        UpdateExpression='SET occupied = :val1',
-        ExpressionAttributeValues={
-            ':val1': {
-                'N': str(occupied)
-            }
-        }
-    )
-    reserved = 1 - occupied
-    return reserved
+    events = __fetch_google_events(room_id, num_events = 1)
 
-def get_credentials():
-    """Gets valid user credentials from storage.
+    any_ongoing_events = any([__is_event_ongoing(event) for event in events])
+    if any_ongoing_events:
+        new_reserved_value = 1
+    else:
+        new_reserved_value = 0
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    # credentials = GoogleCredentials.get_application_default()
-    credential_path = '/tmp/credentials.json'
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    return credentials
+    if new_reserved_value != old_reserved_value:
+        dynamo.update_item(
+                TableName='MotionSign',
+                Key={
+                    'room_id': {
+                        'N': str(room_id)
+                        }
+                    },
+                UpdateExpression='SET reserved = :val1',
+                ExpressionAttributeValues={
+                    ':val1': {
+                        'N': str(new_reserved_value)
+                        }
+                    }
+                )
+    return new_reserved_value
+
+def __is_event_ongoing(event):
+    start = event['start'].get('dateTime', event['start'].get('date'))
+    end = event['end'].get('dateTime', event['end'].get('date'))
+
+    event_start = iso8601.parse_date(start).astimezone(eastern)
+    event_end = iso8601.parse_date(end).astimezone(eastern)
+    current_datetime = datetime.datetime.now(eastern)
+
+    return event_start < current_datetime < event_end
 
 """
 Method to display reservation details for the room
@@ -123,28 +173,7 @@ Method to display reservation details for the room
 """
 def __display(dynamo, params):
     room_id = params['room_id']
-    room_entry = dynamo.get_item(
-            TableName='MotionSign',
-            Key={
-                'room_id': { 'N': room_id }
-            })
-
-    calendar_id = room_entry['Item'].get('calendar_id').get('S')
-
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http, cache_discovery=False)
-
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    eventsResult = service.events().list(
-        calendarId=calendar_id,
-        timeMin=now,
-        maxResults=2,
-        singleEvents=True,
-        orderBy='startTime'
-    ).execute()
-    events = eventsResult.get('items', [])
-
+    events = __fetch_google_events(room_id)
     return __build_str(events)
 
 def __build_event_str(event):
@@ -193,7 +222,6 @@ def lambda_handler(event, context):
     # permission issues
     credential_path = os.path.join(os.environ['LAMBDA_TASK_ROOT'], 'credentials.json')
     shutil.copy(credential_path, "/tmp/credentials.json")
-
 
     operations = {
         'GET': lambda dynamo, x: __get_methods(dynamo, x),
